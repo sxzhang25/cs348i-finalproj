ef080e6767a08afb826faa5f8a75d81e2501c769
Sharon Zhang <zsharonx@gmail.com>
2021-11-10 20:50:21 -0800

    properly initialize TRBA module

--- main_train.py
+++ main_train.py
@@ -4,10 +4,12 @@ from config import get_arguments
 from SinGAN.manipulate import *
 from SinGAN.training import *
 import SinGAN.functions as functions
+from SinGAN.dataloader import create_dataset
 from nltk.corpus import words
 from trba import TRBA
 from trba_utils import AttnLabelConverter
 import cv2
+import torch
 
 
 if __name__ == '__main__':
@@ -37,32 +39,32 @@ if __name__ == '__main__':
   converter = AttnLabelConverter(opt.character)
   opt.num_class = len(converter.character)
 
-  if opt.use_rgb:
-    opt.input_channel = 3
-  trba_net = TRBA(opt).to(opt.device)
-
   print('[INFO] loading TRBA text recognition model...')
+  trba_net = TRBA(opt).to(opt.device)
   model_dict = torch.load(opt.trba, map_location=opt.device)
   # Remove "module." from key names because we are not using DataParallel.
   model_dict = {'.'.join(k.split('.')[1:]): v for k, v in model_dict.items()}
   trba_net.load_state_dict(model_dict)
-  trba_net.eval()
 
-  # if (os.path.exists(dir2save)):
-  #   print('Trained model already exists.')
-  # else:
   try:
     os.makedirs(dir2save)
   except OSError:
     pass
 
   # Get source image.
-  source_img = cv2.imread(f'Images/{opt.input_name}')
+  source_img = cv2.imread(f'{opt.input_dir}/{opt.input_name}')
   height, width, _ = source_img.shape
 
   real = functions.read_image(opt)
   functions.adjust_scales2image(real, opt)
   resnet = models.ResNet34()
-  emb_fixed = functions.generate_text_emb(opt.text, width, height, resnet)
-  train(opt, Gs, words.words(), converter, trba_net, resnet, emb_fixed, height, width, reals, NoiseAmp) # opt, Gs, word_bank, resnet, emb_fixed, reals, NoiseAmp
+  
+  fixed_text_img = functions.render_text(opt.text, width, height, opt.pad)[None, ...]
+  fixed_text_img = torch.Tensor(np.transpose(fixed_text_img, [0, 3, 1, 2]))
+  emb_fixed = functions.generate_text_emb(fixed_text_img, resnet)
+
+  # Create word dataloader.
+  word_bank = create_dataset(words.words(), width, height, opt.pad)
+
+  train(opt, Gs, word_bank, converter, trba_net, resnet, emb_fixed, height, width, reals, NoiseAmp) # opt, Gs, word_bank, resnet, emb_fixed, reals, NoiseAmp
   # SinGAN_generate(Gs, Zs, reals, NoiseAmp, opt)
