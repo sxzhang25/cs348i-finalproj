f2f27d6417821f130cc17d6507af11c009a2b8ed
Sharon Zhang <zsharonx@gmail.com>
2021-11-13 14:56:04 -0800

    modify strings to comply with allowed characters

--- SinGAN/dataloader.py
+++ SinGAN/dataloader.py
@@ -5,23 +5,25 @@ from SinGAN.functions import render_text
 
 
 class WordDataset(Dataset):
-  def __init__(self, word_bank, width, height, pad):
+  def __init__(self, word_bank, character, width, height, pad):
     self.word_bank = word_bank
     self.width = width
     self.height = height
     self.pad = pad
+    self.character = character
 
   def __len__(self):
     return len(self.word_bank)
 
   def __getitem__(self, idx):
     word = self.word_bank[idx].lower()
+    word = ''.join([c for c in word if c in self.character])
     X = render_text(word, self.width, self.height, self.pad)
     X = np.transpose(X, [2, 0, 1])
     return X, word
 
 
-def create_dataset(word_bank, width, height, pad, B=1, shuffle=True):
-  dset = WordDataset(word_bank, width, height, pad)
+def create_dataset(word_bank, character, width, height, pad, B=1, shuffle=True):
+  dset = WordDataset(word_bank, character, width, height, pad)
   loader = DataLoader(dset, batch_size=B, shuffle=shuffle)
   return loader
